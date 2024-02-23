1238a320db91757b7f13da0663a597217f246029
ChristerKnorborg <christerknorborg@hotmail.com>
2024-02-23 13:58:58 +0100

    added constants

--- src/constants.rs
+++ src/constants.rs
@@ -7,67 +7,138 @@ mod mayo1_features {
     pub const  M: usize = 64;
     pub const  O: usize = 8;
     pub const  K: usize = 9;
-    
+    pub const  Q: usize = 16;
+    pub const M_BYTES: usize = 32;
+    pub const O_BYTES: usize = 232;
+    pub const v_BYTES: usize = 29;
+    pub const r_BYTES: usize = 36;
+    pub const P1_BYTES: usize = 54752;
+    pub const P2_BYTES: usize = 14848;
+    pub const P3_BYTES: usize = 1152;
+    pub const CSK_BYTES: usize = 24;
+    pub const ESK_BYTES: usize = 69856;
+    pub const CPK_BYTES: usize = 1168;
+    pub const EPK_BYTES: usize = 70752;
+    pub const SIG_BYTES: usize = 321;
     pub const SALT_BYTES: usize = 24;
     pub const DIGEST_BYTES: usize = 32;
     pub const PK_SEED_BYTES: usize = 16;
-    }
+    pub const SK_SEED_BYTES: usize = 24;
+    pub const L_BYTES: usize = M*(N - O)*O /2;
+}
+
+#[cfg(feature = "mayo2")]
+mod mayo1_features {
+    pub const  N: usize = 78;
+    pub const  M: usize = 64;
+    pub const  O: usize = 18;
+    pub const  K: usize = 4;
+    pub const  Q: usize = 16;
+    pub const M_BYTES: usize = 32;
+    pub const O_BYTES: usize = 540;
+    pub const v_BYTES: usize = 30;
+    pub const r_BYTES: usize = 36;
+    pub const P1_BYTES: usize = 58560;
+    pub const P2_BYTES: usize = 34560;
+    pub const P3_BYTES: usize = 5472;
+    pub const CSK_BYTES: usize = 24;
+    pub const ESK_BYTES: usize = 93684;
+    pub const CPK_BYTES: usize = 5488;
+    pub const EPK_BYTES: usize = 98592;
+    pub const SIG_BYTES: usize = 180;
+    pub const SALT_BYTES: usize = 24;
+    pub const DIGEST_BYTES: usize = 32;
+    pub const PK_SEED_BYTES: usize = 16;
+    pub const SK_SEED_BYTES: usize = 24;
+    pub const L_BYTES: usize = M*(N - O)*O /2;
+}
+
+#[cfg(feature = "mayo3")]
+mod mayo1_features {
+    pub const  N: usize = 99;
+    pub const  M: usize = 96;
+    pub const  O: usize = 10;
+    pub const  K: usize = 11;
+    pub const  Q: usize = 16;
+    pub const M_BYTES: usize = 48;
+    pub const O_BYTES: usize = 445;
+    pub const v_BYTES: usize = 45;
+    pub const r_BYTES: usize = 55;
+    pub const P1_BYTES: usize = 192240;
+    pub const P2_BYTES: usize = 42720;
+    pub const P3_BYTES: usize = 2640;
+    pub const CSK_BYTES: usize = 32;
+    pub const ESK_BYTES: usize = 235437;
+    pub const CPK_BYTES: usize = 2656;
+    pub const EPK_BYTES: usize = 237600;
+    pub const SIG_BYTES: usize = 577;
+    pub const SALT_BYTES: usize = 32;
+    pub const DIGEST_BYTES: usize = 48;
+    pub const PK_SEED_BYTES: usize = 16;
+    pub const SK_SEED_BYTES: usize = 32;
+    pub const L_BYTES: usize = M*(N - O)*O /2;
+}
+
+#[cfg(feature = "mayo5")]
+mod mayo1_features {
+    pub const  N: usize = 133;
+    pub const  M: usize = 128;
+    pub const  O: usize = 12;
+    pub const  K: usize = 12;
+    pub const  Q: usize = 16;
+    pub const M_BYTES: usize = 64;
+    pub const O_BYTES: usize = 726;
+    pub const v_BYTES: usize = 61;
+    pub const r_BYTES: usize = 72;
+    pub const P1_BYTES: usize = 472384;
+    pub const P2_BYTES: usize = 92928;
+    pub const P3_BYTES: usize = 4992;
+    pub const CSK_BYTES: usize = 40;
+    pub const ESK_BYTES: usize = 566078;
+    pub const CPK_BYTES: usize = 5008;
+    pub const EPK_BYTES: usize = 570304;
+    pub const SIG_BYTES: usize = 838;
+    pub const SALT_BYTES: usize = 40;
+    pub const DIGEST_BYTES: usize = 64;
+    pub const PK_SEED_BYTES: usize = 16;
+    pub const SK_SEED_BYTES: usize = 40;
+    pub const L_BYTES: usize = M*(N - O)*O /2;
+}
+
+
 
 
 
 #[cfg(not(feature = "mayo1"))]
 mod other_features {
 
-    // pub const N: usize = 66;
-    pub const N: usize = 8;
-    pub const I: usize = 1;
-    pub const K: usize = 9;
-    // pub const O: usize = 8;
-    pub const O: usize = 2;
 
+    pub const  N: usize = 66;
     pub const  M: usize = 64;
-
-
+    pub const  O: usize = 8;
+    pub const  K: usize = 9;
+    pub const  Q: usize = 16;
+    pub const M_BYTES: usize = 32;
+    pub const O_BYTES: usize = 232;
+    pub const V_BYTES: usize = 29;
+    pub const R_BYTES: usize = 36;
+    pub const P1_BYTES: usize = 54752;
+    pub const P2_BYTES: usize = 14848;
+    pub const P3_BYTES: usize = 1152;
+    pub const CSK_BYTES: usize = 24;
+    pub const ESK_BYTES: usize = 69856;
+    pub const CPK_BYTES: usize = 1168;
+    pub const EPK_BYTES: usize = 70752;
+    pub const SIG_BYTES: usize = 321;
     pub const SALT_BYTES: usize = 24;
-    pub const R_BYTES: usize  = SALT_BYTES;
-    pub const SK_SEED_BYTES: usize = SALT_BYTES;  
-
-    pub const  PK_SEED_BYTES: usize = 16;
-    pub const DIGEST_BYTES: usize= 32;
-
-    // Compact Representiation of secret key
-    pub const CSK_BYTES: usize = SK_SEED_BYTES;
-
-    // ceil( (N - O)*O /2 )
-    pub const O_BYTES: usize = if ((N - O) * O) % 2 == 0 { ((N - O) * O) / 2 } else { ((N - O) * O) / 2 + 1 }; 
-    
-    // ceil( (N - O) /2 )
-    pub const V_BYTES: usize = if (N - O) % 2 == 0 { (N - O) / 2 } else { (N - O) / 2 + 1 }; 
-
-    // Formula for P1 is m * binom(n-o+1, 2) /2
-    pub const P1_BYTES: usize = M * 1711 / 2; // 1711 = binom(66-8+1, 2) 
-
-    pub const P2_BYTES: usize = M*(N - O)*O /2;
-    pub const P3_BYTES: usize = M * 6 /2;
+    pub const DIGEST_BYTES: usize = 32;
+    pub const PK_SEED_BYTES: usize = 16;
+    pub const SK_SEED_BYTES: usize = 24;
     pub const L_BYTES: usize = M*(N - O)*O /2;
-
-    // Expanded Representation of Secret key
-    pub const ESK_BYTES: usize = SK_SEED_BYTES + O_BYTES + P1_BYTES +  L_BYTES; 
-
-    // Compact Representation of Public key
-    pub const CPK_BYTES: usize = PK_SEED_BYTES + P3_BYTES;
-
-    // Expanded Representation of Public key
-    pub const EPK_BYTES: usize = P1_BYTES + P2_BYTES + P3_BYTES;
-
-    // ceil( (N*K / 2) )  + SALT_BYTES
-    pub const SIG_BYTES: usize = if (N*K  % 2) == 0 { (N*K / 2) + SALT_BYTES } else { (N*K / 2) + SALT_BYTES + 1 };
 
 
     // Compact representation of irreducible polynomial [z^0 + z^1 + z^2 + z^3 + z^m]
     pub const F_Z: [u8; 5] = [8, 0, 2, 8, 0]; // f(z) =  z^64         + x^3*z^3 + x*z^2         + x^3
-
-    
 }
 
 
