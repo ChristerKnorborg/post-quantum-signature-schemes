3a8970802313dc25aaa12828103e1ca93ddbc6c4
mathias <mathiasmak@live.dk>
2024-03-07 09:35:15 +0100

    merged and setup benchmarking

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+pub mod bitsliced_functionality;
+pub mod constants;
+pub mod crypto_primitives;
+pub mod finite_field;
+pub mod mayo_functionality;
+pub mod read_kat_file;
+pub mod sample;
+pub mod utils;
+pub mod genKAT {
+    pub mod bindings;
+}
