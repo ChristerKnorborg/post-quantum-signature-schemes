dc7519652b75066deee20186c953cdc138c0b083
sgrarup <sgrarup@gmail.com>
2024-03-08 12:33:01 +0100

    created branch

--- src/benches/my_benchmark.rs
+++ src/benches/my_benchmark.rs
@@ -1,5 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion};
-use criterion_cycles_per_byte::CyclesPerByte;
+// use criterion_cycles_per_byte::CyclesPerByte;
 use gnuplot::{Caption, Color, Figure};
 
 
