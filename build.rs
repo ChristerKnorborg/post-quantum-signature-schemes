6cf1f0661bfa5e539f12a74eb111b79848579546
sgrarup <sgrarup@gmail.com>
2024-04-18 10:52:58 +0200

    added sytemcall randomness

--- build.rs
+++ build.rs
@@ -1,10 +1,20 @@
 // build.rs
 fn main() {
-    cc::Build::new()
-        .file("src/genkat/randombytes_ctrdrbg.c")
-        .file("src/genkat/mem.c")
+    let mut build = cc::Build::new();
+
+    // Use conditional compilation based on the feature
+    if std::env::var("CARGO_FEATURE_SYSTEM_AES").is_ok() {
+        println!("cargo:info=Using system AES");
+        build.file("src/genkat/randombytes_system.c");
+    } else {
+        println!("cargo:info=Using default AES");
+        build.file("src/genkat/randombytes_ctrdrbg.c");
+    }
+
+    build.file("src/genkat/mem.c")
         .file("src/genkat/aes_c.c")
         .file("src/genkat/fips202.c")
         .flag("-O2")
+        .flag("-DENABLE_AESNI=ON ")
         .compile("randombytes_nist");
 }
