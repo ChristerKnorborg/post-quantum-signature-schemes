9ac4a33df905cc2ba86fe6845a3544ed418a7fa7
sgrarup <sgrarup@gmail.com>
2024-05-14 15:55:35 +0200

    cursed

--- build.rs
+++ build.rs
@@ -5,14 +5,14 @@ fn main() {
     // Use conditional compilation based on the feature
     if std::env::var("CARGO_FEATURE_SYSTEM_AES").is_ok() {
         println!("cargo:info=Using system AES");
-        build.file("src/genkat/randombytes_system.c");
+        build.file("src/genkat/aes128ctr.c");
     } else {
         println!("cargo:info=Using default AES");
-        build.file("src/genkat/randombytes_ctrdrbg.c");
+        build.file("src/genkat/aes_c.c");
     }
 
     build.file("src/genkat/mem.c")
-        .file("src/genkat/aes_c.c")
+        .file("src/genkat/randombytes_ctrdrbg.c")
         .file("src/genkat/fips202.c")
         .file("src/arm_neon_intrinsic/armv8_intrinsic.c")
         .flag("-O3")
