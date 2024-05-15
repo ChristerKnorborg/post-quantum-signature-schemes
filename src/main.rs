0a26166aab0f9bf65b1813bf0e521656a0cc7bc2
mathias <mathiasmak@live.dk>
2024-05-15 15:05:33 +0200

    made benchmark

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use lib::write_and_compare_kat_file::write_and_compare_kat_file;
 
+use lib::own_benchmark::benchmark;
+
 fn main() {
     write_and_compare_kat_file();
+
+    benchmark(1000);
+
 }
