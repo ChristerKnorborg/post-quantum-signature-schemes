7b0d9d5e44c25c4855d87c0fb64ae6924be0c47e
mathias <mathiasmak@live.dk>
2024-05-16 12:42:22 +0200

    bench setup

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ pub mod finite_field;
 pub mod mayo_functionality;
 pub mod write_and_compare_kat_file;
 pub mod sample;
+pub mod benchmark;
 pub mod utils;
 pub mod genkat {
     pub mod bindings;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
-
+use lib::constants::{M, VERSION};
 use lib::write_and_compare_kat_file::write_and_compare_kat_file;
 
+use lib::benchmark::benchmark;
+
 fn main() {
-    write_and_compare_kat_file();
+
+
+    #[cfg(not(feature = "bench"))]
+    {
+        write_and_compare_kat_file();
+    }
+
+
+    #[cfg(feature = "bench")]
+    {
+        benchmark(1000);
+    }
+
+
 }
