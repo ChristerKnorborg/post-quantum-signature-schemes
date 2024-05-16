f3753c768f84f51939eecff1ffa89664c64ae440
mathias <mathiasmak@live.dk>
2024-05-16 10:49:25 +0200

    copied bench

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
+use lib::constants::{M, VERSION};
 use lib::write_and_compare_kat_file::write_and_compare_kat_file;
 
-fn main() {
-    write_and_compare_kat_file();
- }
+use lib::own_benchmark::benchmark;
 
+fn main() {
+
+    println!("{}" , M);
+    println!("{}" , VERSION);
+
+    #[cfg(not(feature = "bench"))]
+    {
+        write_and_compare_kat_file();
+    }
+
+
+    #[cfg(feature = "bench")]
+    {
+        println!("bench");
+        benchmark(1000);
+    }
+
+
+}
