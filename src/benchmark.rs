6f27b0be264f3a597ca6f669a216ba4542308154
mathias <mathiasmak@live.dk>
2024-05-20 10:35:51 +0200

    bench cycle setup

--- src/benchmark.rs
+++ src/benchmark.rs
@@ -6,14 +6,13 @@ use std::path::Path;
 use std::fs::OpenOptions;
 use std::io::Write;
 use crate::constants::{COMPARE_FILE_NAME, SIG_BYTES, VERSION};
+use crate::utils::bytes_to_hex_string;
 
 
 use crate::crypto_primitives::{
-    safe_random_bytes, safe_random_bytes_init
+    safe_aes_128_ctr, safe_random_bytes, safe_random_bytes_init, safe_shake256,
 };
-use crate::mayo_functionality::{
-    api_sign, api_sign_open, compact_key_gen, expand_pk, expand_sk
-};
+use crate::mayo_functionality::{api_sign, api_sign_open, compact_key_gen, expand_pk, expand_sk};
 
 
 use csv::Writer;
@@ -25,7 +24,7 @@ pub fn benchmark(amount_of_iterations: i32) -> Result<(), Box<dyn Error>> {
 
     println!("\nRUNNING BENCHMARKS FOR {} \n", VERSION);
     
-let implementation_variant = "bitsliced";
+let implementation_variant = "Rust_bitsliced";
 
     let base_dir = "benchmark_result";
     if !std::path::Path::new(base_dir).exists() {
@@ -97,7 +96,7 @@ let implementation_variant = "bitsliced";
 
         //this loop runs the benchmark for the expand sk function
         let mut total_duration_expand_sk = Duration::new(0, 0);
-        for _ in 0..amount_of_iterations{
+        for i in 0..amount_of_iterations{
 
         let (_ , csk) = compact_key_gen();
     
@@ -105,7 +104,7 @@ let implementation_variant = "bitsliced";
         let start_expand_sk = Instant::now();
     
 
-        expand_sk(csk);
+            expand_sk(csk);
         //  expand_pk(cpk);
     
         //  let signature = api_sign(message_vec, csk);
@@ -123,7 +122,7 @@ let implementation_variant = "bitsliced";
         
         //this loop runs the benchmark for the expand_pk function
         let mut total_duration_expand_pk = Duration::new(0, 0);
-        for _ in 0..amount_of_iterations{
+        for i in 0..amount_of_iterations{
 
         let (cpk , _) = compact_key_gen();
     
@@ -148,7 +147,7 @@ let implementation_variant = "bitsliced";
 
         //this loop runs the benchmark for the sign function
         let mut total_duration_sign = Duration::new(0, 0);
-        for _ in 0..amount_of_iterations{
+        for i in 0..amount_of_iterations{
 
             let (_, csk) = compact_key_gen();
             let mut message = [0u8; 32];
@@ -169,7 +168,7 @@ let implementation_variant = "bitsliced";
 
         //this loop runs the benchmark for the verify function
         let mut total_duration_verify = Duration::new(0, 0);
-        for _ in 0..amount_of_iterations{
+        for i in 0..amount_of_iterations{
 
             let (cpk, csk) = compact_key_gen();      
             let mut message = [0u8; 32];
@@ -190,13 +189,52 @@ let implementation_variant = "bitsliced";
         let final_average_duration_verify = total_duration_verify / amount_of_iterations.try_into().unwrap();
 
 
+       let mut res_average_duration_keygen = format_duration_as_nanos(&final_average_duration_keygen);
+       let mut res_average_duration_expand_sk =format_duration_as_nanos(&final_average_duration_expand_sk) ;// Replace with format_duration(duration_expand_sk) when enabled
+       let mut res_average_duration_expand_pk = format_duration_as_nanos(&final_average_duration_expand_pk);
+       let mut res_average_duration_sign = format_duration_as_nanos(&final_average_duration_sign);
+       let mut res_average_duration_verify = format_duration_as_nanos(&final_average_duration_verify);
+        
+        #[cfg(feature = "CCM1")]
+        {
+            println!("CCM1 is enabled");
+
+            let cpu_speed_hz = 3.2*1e9;
+
+
+
+            res_average_duration_keygen = format_duration_as_string(&(cpu_speed_hz * (final_average_duration_keygen.as_nanos() as f64 / 1e9) as f64));
+            res_average_duration_expand_sk = format_duration_as_string(&(cpu_speed_hz * (final_average_duration_expand_sk.as_nanos() as f64 / 1e9) as f64));
+            res_average_duration_expand_pk = format_duration_as_string(&(cpu_speed_hz * (final_average_duration_expand_pk.as_nanos() as f64 / 1e9) as f64));
+            res_average_duration_sign = format_duration_as_string(&(cpu_speed_hz * (final_average_duration_sign.as_nanos() as f64 / 1e9) as f64));
+            res_average_duration_verify = format_duration_as_string(&(cpu_speed_hz * (final_average_duration_verify.as_nanos() as f64 / 1e9) as f64));
+
+
+
+        }
+
+        #[cfg(feature = "CCODROID-C4")]
+        {
+            println!("CCODROID-C4 is enabled");
+
+            
+            let cpu_speed_hz = 1.91*1e9;
+
+            res_average_duration_keygen = format_duration_as_string(&(cpu_speed_hz * (final_average_duration_keygen.as_nanos() as f64 / 1e9) as f64));
+            res_average_duration_expand_sk = format_duration_as_string(&(cpu_speed_hz * (final_average_duration_expand_sk.as_nanos() as f64 / 1e9) as f64));
+            res_average_duration_expand_pk = format_duration_as_string(&(cpu_speed_hz * (final_average_duration_expand_pk.as_nanos() as f64 / 1e9) as f64));
+            res_average_duration_sign = format_duration_as_string(&(cpu_speed_hz * (final_average_duration_sign.as_nanos() as f64 / 1e9) as f64));
+            res_average_duration_verify = format_duration_as_string(&(cpu_speed_hz * (final_average_duration_verify.as_nanos() as f64 / 1e9) as f64));
+        }
+
+
     wtr.write_record(&[
         &VERSION.to_string(),
-        &format_duration_as_nanos(final_average_duration_keygen),
-        &format_duration_as_nanos(final_average_duration_expand_sk), // Replace with format_duration(duration_expand_sk) when enabled
-        &format_duration_as_nanos(final_average_duration_expand_pk),
-        &format_duration_as_nanos(final_average_duration_sign),
-        &format_duration_as_nanos(final_average_duration_verify),
+        &res_average_duration_keygen,
+        &res_average_duration_expand_sk, // Replace with format_duration(duration_expand_sk) when enabled
+        &res_average_duration_expand_pk,
+        &res_average_duration_sign,
+        &res_average_duration_verify,
     ])?;
 
 
@@ -206,6 +244,10 @@ let implementation_variant = "bitsliced";
 
 }
 
-fn format_duration_as_nanos(dur: Duration) -> String {
+fn format_duration_as_nanos(dur: &Duration) -> String {
     format!("{:.5?}", dur.as_nanos())
+}
+
+fn format_duration_as_string(dur: &f64) -> String {
+    format!("{:.0?}", dur)
 }
