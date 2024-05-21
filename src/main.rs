acbdff9f3c63f470e3651aa7bc05defe2cc1906f
ChristerKnorborg <christerknorborg@hotmail.com>
2024-05-21 15:24:37 +0200

    cleanup avx adatp impl. Rdy for benchmark

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
-use lib::write_and_compare_kat_file::write_and_compare_kat_file;
-
-#[allow(unused_imports)]
-use lib::benchmark::benchmark;
 
 fn main() {
 
 
     #[cfg(not(feature = "bench"))]
     {
+        use lib::write_and_compare_kat_file::write_and_compare_kat_file;
         write_and_compare_kat_file();
     }
 
 
     #[cfg(feature = "bench")]
     {
+        use lib::benchmark::benchmark;
         let _ = benchmark(1000);
     }
 
