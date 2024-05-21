fcd33df1ff486257195cac06c82a5d6c8d3e22fc
ChristerKnorborg <christerknorborg@hotmail.com>
2024-05-21 14:33:45 +0200

    setup for benchmarks

--- build.rs
+++ build.rs
@@ -1,17 +1,20 @@
-// build.rs
 fn main() {
     let mut build = cc::Build::new();
 
-    // Use conditional compilation based on the feature
-    if std::env::var("CARGO_FEATURE_SYSTEM_AES").is_ok() {
-        println!("cargo:info=Using system AES");
+    // Check if aes_neon is enabled
+    if std::env::var("CARGO_FEATURE_AES_NEON").is_ok() {
+        println!("cargo:info=Using AES with NEON intrinsics");
         build.file("src/genkat/aes_arm.c");
+        build.flag_if_supported("-march=armv8-a+crypto"); // Enable Cryptography extensions
+
     } else {
+
         println!("cargo:info=Using default AES");
         build.file("src/genkat/aes_c.c");
     }
-       build.file("src/genkat/mem.c")
-        .file("src/genkat/randombytes_ctrdrbg.c")
+
+    build.file("src/genkat/randombytes_ctrdrbg.c")
+        .file("src/genkat/mem.c")
         .file("src/genkat/fips202.c")
         .file("src/arm_neon_intrinsic/armv8_intrinsic.c")
         .flag("-O3")
